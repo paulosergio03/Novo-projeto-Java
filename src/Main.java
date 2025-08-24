0e237ed14a792b76f05dffcc76b9d8d27af15f84
Paulo Sérgio <paulosergioadm9@gmail.com>
2025-08-24 13:25:56 -0300

    Alteração

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
         String nome = "Paulo Sérgio";
 
         System.out.println(nome);
+
+        //Classes Wrapper
+        Byte idade = 120;
+        Short estoque = 32767;
+        Integer numero = 2147483647;
+        Long contaCorrente = 9223372036854775807L;
+        Double dolar = 100.500;
+        Boolean ativado = true;
+
+
     }
 }
